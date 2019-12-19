bec4157698e043b0830b0b8084f92e92856c52d4
Diogo Martins <nunodiogomartins@gmail.com>
2019-12-19 11:53:19 +0000

    Set up routes
    Create placeholders for components, set up router and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+import AddProduct from './components/AddProduct';
+import ProductDetail from './components/ProductDetail';
+import ProductList from './components/ProductList';
+
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-		</div>
+		<Router>
+            <div>
+                <aside>
+                    <Link to={'/'}>Products</Link>
+                    <Link to={'/add-product'}>Add product</Link>
+                </aside>
+            </div>
+
+            <main>
+                <Route exact path="/" component={ProductList} />
+                <Route path="/add-product" component={AddProduct} />
+                <Route path="/product/:id" component={ProductDetail} />
+            </main>
+        </Router>
 	);
 }
 
---
+++ src/components/AddProduct.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddProduct = props => {
+    return <div>Add Product</div>
+}
+
+export default AddProduct;
